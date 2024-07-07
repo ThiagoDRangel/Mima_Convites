e391de21dcf7c0eab92105f0f7aa865a092d1125
ThiagoDRangel <info.tec.campos@gmail.com>
2024-07-07 19:37:01 -0300

    feat: implements Header component

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -1,10 +1,20 @@
+import './styles.css';
+
+
 function Social () {
   return (
-    <ul>
-        <li>Instagram</li>
-        <li>Youtube</li>
-        <li>WhattsApp</li>
-    </ul>
+    <div className="container-social">
+      <ul className="container-social-left">
+        <li>QUEM SOMOS</li>
+        <li>FALE CONOSCO</li>
+        <li>PERGUNTAS FREQUENTES</li>
+      </ul>
+      <ul className="container-social-right">
+          <li>WhatsApp</li>
+          <li>Email</li>
+          <li>Instagram</li>
+      </ul>
+    </div>
   );
 }
 
