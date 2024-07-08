4eea265a5a4f060e7b5f4033dd44e55d1035b50e
ThiagoDRangel <info.tec.campos@gmail.com>
2024-07-08 18:47:24 -0300

    feat: implements flexbox in Cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,10 +4,16 @@ import image from '../../assets/images/fundo.png';
 function Card () {
   return (
     <div className="container-img">
-      <img src={image} alt="imagem de teste" />
-      <img src={image} alt="imagem de teste" />
+      <img className="img-1" src={image} alt="imagem de teste" />
+      <div className="container-img-2">
+        <img className="img-2" src={image} alt="imagem de teste" />
+        <div className="container-img-3">
+          <img className="img-3" src={image} alt="imagem de teste" />
+          <img className="img-4" src={image} alt="imagem de teste" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Card;
